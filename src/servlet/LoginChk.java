17bc9ee2c20fcb8fa5bb416abbbcc3be10ba4bca
saito0000 <oscar.prairie@gmail.com>
2022-11-11 10:49:41 +0900

    2022/11/11 saito

--- src/servlet/LoginChk.java
+++ src/servlet/LoginChk.java
@@ -15,28 +15,28 @@ import model.dao.UserDAO;
 
 @WebServlet("/LoginChk")
 public class LoginChk extends HttpServlet{
-	
+
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
 		req.setCharacterEncoding("UTF-8");
-		
+		// uooooo
 		// セッション起動
 		HttpSession session = req.getSession();
-		
+
 		// リクエストパラメータを取得
 		String employeeNum = req.getParameter("employeeNum");
 		String password = req.getParameter("password");
-		
+
 		// データベースへの接続
 		UserDAO userDao = UserDAO.getInstance();
-		
+
 		Map<String, Object> reMap = new HashMap<String, Object>();
 		try {
 			// DB接続
 			userDao.dbConnect();
 			// ステートメント作成
 			userDao.createSt();
-			
+
 			reMap = userDao.loginUser(employeeNum, password);
 		}catch (Exception e ) {
 			e.printStackTrace();
@@ -44,10 +44,10 @@ public class LoginChk extends HttpServlet{
 			// DB切断
 			userDao.dbDiscon();
 		}
-		
+
 		// セッションにパラメータをセット
 		session.setAttribute("name", reMap.get("employeeName"));
-		
+
 		// 指定した画面へ遷移
 		req.getRequestDispatcher("/portal.jsp").forward(req, res);
 	}
